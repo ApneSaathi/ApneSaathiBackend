2fc9cc44eeee1211153962daf3ce26f94cf34be1
ApneSaathi <66403613+ApneSaathi@users.noreply.github.com>
2020-08-17 17:39:03 +0530

    added SrCitizenResonse model for the backend portal

---
+++ SrCitizenResponse.java
@@ -0,0 +1,17 @@
+package com.kef.org.rest.model;
+
+import java.util.List;
+
+public class SrCitizenResponse {
+
+	List<SeniorCitizen> srCitizenList;
+
+	public List<SeniorCitizen> getSrCitizenList() {
+		return srCitizenList;
+	}
+
+	public void setSrCitizenList(List<SeniorCitizen> srCitizenList) {
+		this.srCitizenList = srCitizenList;
+	}
+	
+}
