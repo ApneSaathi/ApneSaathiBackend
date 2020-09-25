7ba41c5ea426af4a6d791338a9b48747de89c258
jessycaresyou@gmail.com <jessycaresyou@gmail.com>
2020-09-25 11:51:17 +0530

    removed unused import

--- developer-parvathy/VolunteerApp/src/main/java/com/kef/org/rest/service/SeniorCitizenService.java
+++ developer-parvathy/VolunteerApp/src/main/java/com/kef/org/rest/service/SeniorCitizenService.java
@@ -12,7 +12,6 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
